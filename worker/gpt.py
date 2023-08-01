174b61e99a5d10efa6a91a6ee35dbeeee1171b59
Sophia <sophia@perzan.dev>
2023-08-01 17:59:38 -0400

    Edited GPT prompts

--- worker/gpt.py
+++ worker/gpt.py
@@ -10,22 +10,40 @@ class GPT:
             self.model = "gpt-4"
 
     def getGender(self, text):
-        instructions = "Given the following reddit post, determine the gender of the poster. Use the context of the post to aid you. If it is ambiguous, reply with the most likely answer. Reply with just a single letter, either M or F."
-        return openai.ChatCompletion.create(model=self.model, messages=[{"role": "system", "content": instructions},
-                                                                        {"role": "user", "content": text}], temperature=0.2).choices[0].message.content
+        instructions = "From the given text, determine the poster's gender. Use the context provided by the text. If the gender is ambiguous, reply with the most probable gender. Respond with a single letter: 'M' for Male or 'F' for Female."
+        return openai.ChatCompletion.create(
+            model=self.model,
+            messages=[
+                {"role": "system", "content": instructions},
+                {"role": "user", "content": text}
+            ],
+            temperature=0.2
+        ).choices[0].message.content
 
     def expandAcronymsAndAbbreviations(self, text, language="english"):
-        sharedInstructions = "edit it so that the abbreviations/acronyms are expanded, and correct grammar mistakes/correct for general ease of understanding. A text to speech program will use this as input, so make sure the output will be easily processed by the program. Add additional punctuation if necessary to make the speech flow better. You may leave commonly understood acronyms/abbreviations unexpanded."
+        sharedInstructions = "Expand the abbreviations and acronyms in the text, correct grammar mistakes, and enhance the overall readability. Since the output will be used as input for a text-to-speech program, ensure it can be processed easily. Add punctuation as necessary for smooth speech flow. You can leave commonly understood acronyms and abbreviations as they are."
+
         if language != "english":
-            instructions = "Translate the following reddit post to " + \
-                language+", then "+sharedInstructions + \
-                " Then expand/convert all characters that are not letters, into the equivalent word/letter representation in the target language. No numerical characters can remain in the text."
+            instructions = f"Translate the following Reddit post to {language}, then {sharedInstructions}. Moreover, replace all non-letter characters with their equivalent word or letter representation in the target language. No numerical characters should remain in the text."
         else:
-            instructions = "Given the following reddit post, "+sharedInstructions
-        return openai.ChatCompletion.create(model=self.model, messages=[{"role": "system", "content": instructions},
-                                                                        {"role": "user", "content": text}], temperature=0.1).choices[0].message.content
+            instructions = f"Given the following Reddit post, {sharedInstructions}"
+
+        return openai.ChatCompletion.create(
+            model=self.model,
+            messages=[
+                {"role": "system", "content": instructions},
+                {"role": "user", "content": text}
+            ],
+            temperature=0.1
+        ).choices[0].message.content
 
     def getSubtitles(self, text):
-        instructions = "Given the following transcript, expand/convert all characters that are not letters, into the equivalent word/letter representation."
-        return openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "system", "content": instructions},
-                                                                             {"role": "user", "content": text}], temperature=0.1).choices[0].message.content
+        instructions = "Given the following transcript, replace all non-letter characters and numbers with their corresponding word or letter representation."
+        return openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": instructions},
+                {"role": "user", "content": text}
+            ],
+            temperature=0.1
+        ).choices[0].message.content
